9f4a94df9ae5f878c27a1eddab11bb50a562c73c
Christopher Marek <christophermarek42@gmail.com>
2016-11-08 10:32:24 -0500

    Add files via upload

---
+++ creatorwindow.cpp
@@ -0,0 +1,14 @@
+-#include "creatorwindow.h"		
+ -#include "ui_creatorwindow.h"		
+ -		
+ -creatorWindow::creatorWindow(QWidget *parent) :		
+ -    QMainWindow(parent),		
+ -    ui(new Ui::creatorWindow)		
+ -{		
+ -    ui->setupUi(this);		
+ -}		
+ -		
+ -creatorWindow::~creatorWindow()		
+ -{		
+ -    delete ui;		
+ -}
